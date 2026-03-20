7b680cbe2ce9dc50778e91fca49e22df2709bbcf
cp-20 <n.naoki.1587@gmail.com>
2026-03-20 17:22:07 +0900

    変な gzip 消した

--- application/client/src/utils/fetchers.ts
+++ application/client/src/utils/fetchers.ts
@@ -1,5 +1,3 @@
-import { gzip } from "pako";
-
 async function parseJSONResponse<T>(response: Response): Promise<T> {
   if (!response.ok) {
     throw new Error(
@@ -43,13 +41,10 @@ export async function sendFile<T>(url: string, file: File): Promise<T> {
 
 export async function sendJSON<T>(url: string, data: object): Promise<T> {
   const jsonString = JSON.stringify(data);
-  const uint8Array = new TextEncoder().encode(jsonString);
-  const compressed = gzip(uint8Array);
 
   const response = await fetch(url, {
-    body: compressed,
+    body: jsonString,
     headers: {
-      "Content-Encoding": "gzip",
       "Content-Type": "application/json",
     },
     method: "POST",
